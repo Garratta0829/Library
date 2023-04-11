39210b7a88f19d6c45cbeacc5696c83b300e93f6
Andrew Garratt <Andrew.GarrattN@outlook.com>
2023-04-10 17:20:45 -0700

    rendering library works

--- library.js
+++ library.js
@@ -1,20 +1,24 @@
 const addButton = document.querySelector('.add-button')
 const closeButton = document.querySelector('.modal-close')
-// const modal = document.querySelector('.modal')
 const modal = document.querySelector('.modal')
+const form = document.querySelector('.modal-container')
+const addBook = document.querySelector('.add-book')
+const titleInput = document.querySelector('.title-input')
+const authorInput = document.querySelector('.author-input')
+const pagesInput = document.querySelector('.pages-input')
+let title = titleInput.value
+let author = authorInput.value
+let pages = pagesInput.value
+
+const body = document.querySelector('.body')
+
 
 
 let myLibrary = []
 
 addButton.addEventListener('click', () => {
-   
-    // modal.classList.add('active')
     openModal(modal)
 })
-
-// function addDisplay() {
-//     modal.show();
-// }
 
 closeButton.addEventListener('click', () => {
     closeModal(modal)
@@ -29,6 +33,27 @@ function closeModal(modal) {
     modal.classList.remove('active')
 }
 
+// addBook.addEventListener('click', newBook )
+
+// function newBook(title, author, pages) {
+//     console.log(Book(title, author, pages))
+    
+//    console.log(new Book)
+// }
+
+
+
+addBook.addEventListener('click', () => {
+    title = titleInput.value
+    author = authorInput.value
+    pages = pagesInput.value
+    const book = new Book(title, author, pages)
+    myLibrary.push(book)
+    console.log(myLibrary)
+    renderLibrary(myLibrary)
+    modal.classList.remove('active')
+})
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -36,3 +61,37 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+function renderLibrary(myLibrary) {
+    
+    for (let i = 0; i < myLibrary.length; i++) {
+        const div = document.createElement('div')
+        div.innerHTML = `${title} ${author} ${pages}`
+        div.style.border = '1px solid black'
+
+    body.appendChild(div)
+    }
+    
+    
+}
+
+
+
+
+// const div = document.createElement('div')
+// div.innerHTML = 'Im a div'
+// div.style.border = '1px solid black'
+
+// body.appendChild(div)
+
+// const div2 = document.createElement('div')
+// div2.innerHTML = 'Im a div'
+// div2.style.border = '1px solid black'
+
+// body.appendChild(div2)
+
+// const div3 = document.createElement('div')
+// div3.innerHTML = 'Im a div'
+// div3.style.border = '1px solid black'
+// div3.style.height = '400px'
+
+// body.appendChild(div3)
